d1650d576b0eed53cf9fae251a16ee5291e17596
AMEntwistle <aentwistle@sonymusic-pde.com>
2025-04-09 11:35:44 +0100

    Initial commit

---
+++ locustfile.py
@@ -0,0 +1,47 @@
+import os
+from concurrent.futures import ThreadPoolExecutor
+
+from locust import HttpUser, task, between
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class GatewayUser(HttpUser):
+    wait_time = between(1, 5)
+    host = os.environ.get('GRAPHQL_URL')
+
+    def __init__(self, environment):
+        super().__init__(environment)
+        self.bearer_token = os.environ.get('BEARER_TOKEN')
+
+    def send_graphql_request(self, query_file_name, variables):
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f'{self.bearer_token}',
+            "apollographql-client-name": "AbacusPerformanceTest",
+            "orchard-profile-type": "AbacusProfile",
+            "orchard-profile-uuid": "277f57b2-937c-42d0-8cdb-7f05d3f64155",
+        }
+        with open(f'graphqlQueries/{query_file_name}.gql', 'r') as file:
+            query = file.read()
+        body = {
+            "query": query,
+            "variables": variables
+        }
+        self.client.post(self.host, json=body, headers=headers, name=query_file_name)
+
+    @task
+    def visit_contract_page(self):
+        requests = [
+            ('getContract', {"contractId": "544622", "groupAdmin": "PRESENTATIONAL"}),
+            ('getTransactionTypesWithGroups', {"groupAdmin": "PRESENTATIONAL"}),
+        ]
+
+        with ThreadPoolExecutor() as executor:
+            futures = [
+                executor.submit(self.send_graphql_request, query_file, variables)
+                for query_file, variables in requests
+            ]
+            for future in futures:
+                future.result()
